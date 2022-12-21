c694877a0bd4078ed4b25428883690d3e7699644
samicsc0 <h4ck4n0nyt@gmail.com>
2022-12-21 15:41:27 +0300

    domino piling

--- Codeforces/Domino Piling/DominoPiling.java
+++ Codeforces/Domino Piling/DominoPiling.java
@@ -1,14 +1,10 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package data.structure.and.algorith;
-
-/**
- *
- * @author pc
- */
+import java.util.Scanner;
 public class DominoPiling {
-    
+    int dp(){
+        Scanner sc = new Scanner(System.in);
+        int num1 = sc.nextInt();
+        int num2 = sc.nextInt();
+        return (num1 * num2) / 2;
+    }
 }
